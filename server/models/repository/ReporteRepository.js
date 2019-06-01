a87b945f820a4ecaad95ace90f39c0bc66df597b
anderson420 <44132519+anderson420@users.noreply.github.com>
2019-06-01 05:09:40 -0500

    dsd

--- server/models/repository/ReporteRepository.js
+++ server/models/repository/ReporteRepository.js
@@ -19,6 +19,21 @@ class ReporteRepository {
              return reportes;
         });
     }
+    async getReportesById(id){
+        return await con.query(`
+            SELECT * FROM reporte  where reporte.idreporte=${id};
+        `).then(function(result){
+
+            var reportes = [];
+            var temp;
+            
+            result.forEach(element => {
+                temp = new Reporte(element.idreporte, element.fecha, element.tipo_reporte_idtipo_reporte );
+                reportes.push(temp);
+            });
+             return reportes;
+        });
+    }
     async SetReporte(ob){
         return await con.query(`
             INSERT INTO reporte (idreporte, fecha, tipo_reporte_idtipo_reporte)
