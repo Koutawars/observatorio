38b8531585453eea75edef036270096f1bf87279
anderson420 <44132519+anderson420@users.noreply.github.com>
2019-05-31 23:00:34 -0500

    v:

---
+++ server/models/identity/Vision.js
@@ -0,0 +1,10 @@
+
+
+class Vision {
+    constructor(idvision, nivel){
+        this.idvision = idvision;
+        this.nivel = nivel;
+    }
+}
+
+module.exports = Vision;
---
+++ server/models/repository/VisionRepository.js
@@ -0,0 +1,26 @@
+const con = require('../../connection.js');
+const Vision = require('../identity/Vision.js');
+
+
+class VisionRepository {
+
+    async getObservaciones(id){
+        return await con.query(`
+            SELECT * FROM vision;
+        `).then(function(result){
+
+            var visiones = [];
+            var temp;
+            
+            result.forEach(element => {
+                temp = new Vision(element.idvision, element.nivel);
+                visiones.push(temp);
+            });
+             return visiones;
+        });
+    }
+
+
+
+}
+module.exports = VisionRepository;
